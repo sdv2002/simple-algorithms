053f19995fc7f8c31fa1dee111682f841dfcb923
sdv2002 <sdv2002@yandex.ru>
2019-08-22 16:26:37 +0300

    added expression in postfix form

--- stack.py
+++ stack.py
@@ -57,13 +57,74 @@ def sort_stack(stack):
 
 
 def revers_string(array):
-    """Reverse a string using a stack"""
+    """Reverse a string using a stack."""
     stack = []
     while array:
         stack.append(array.pop())
     return stack
 
 
+def conversion_to_postfix(string):
+    """Conversion from infix to postfix form."""
+    operators_priority = {'*': 3,
+                          '/': 3,
+                          '+': 2,
+                          '-': 2,
+                          '(': 1
+                          }
+    stack = []
+    postfix_list = []
+    infix_list = string.split()
+    for token in infix_list:
+        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or token.isdigit():
+            postfix_list.append(token)
+        elif token == '(':
+            stack.append(token)
+        elif token == ')':
+            operand = stack.pop()
+            while operand != '(':
+                postfix_list.append(operand)
+                operand = stack.pop()
+        elif token in operators_priority:
+            if stack:
+                operand = stack.pop()
+                if operators_priority[operand] >= operators_priority[token]:
+                    postfix_list.append(operand)
+                else:
+                    stack.append(operand)
+            stack.append(token)
+    while stack:
+        postfix_list.append(stack.pop())
+    return ' '.join(postfix_list)
+
+
+def calculation_postfix(string):
+    """Calculation an expression in postfix form."""
+    stack = []
+    postfix_list = string.split()
+    for token in postfix_list:
+        if token.isdigit():
+            stack.append(int(token))
+        if token in '*/+-':
+            operand_2 = stack.pop()
+            operand_1 = stack.pop()
+            stack.append(compute(operand_1, operand_2, token))
+    return stack.pop()
+
+
+def compute(x, y, operator):
+    if operator == '*':
+        return x * y
+    elif operator == '/':
+        return x / y
+    elif operator == '+':
+        return x + y
+    else:
+        return x - y
+
+
 if __name__ == '__main__':
-    a = [3, 2, 10, 0, 7]
-    print(revers_string(a))
+    a = '( 2 + 10 ) / 2'
+    b = conversion_to_postfix(a)
+    print(a, '<=>', b)
+    print(calculation_postfix(b))
