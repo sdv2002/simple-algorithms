789ec0960a30bb12423fb0c34820ef9ea6a83015
sdv2002 <sdv2002@yandex.ru>
2019-08-23 13:30:37 +0300

    added transposition and binary numbers in queue

--- queue.py
+++ queue.py
@@ -1,4 +1,5 @@
 from collections import deque
+from math import log2
 
 
 class StackThroughQueue:
@@ -26,14 +27,25 @@ class StackThroughQueue:
             return None
 
 
+def transposition(queue: deque, k):
+    """Rearranges the first k elements of the queue."""
+    for _ in range(k):
+        queue.appendleft(queue.pop())
+
+
+def generate_binary_numbers(n):
+    """Generate binary numbers from 1 to n using a queue."""
+    digits = int(log2(n)) + 1
+    list_of_numbers = []
+    for x in range(1, n + 1):
+        queue = deque()
+        while x:
+            queue.appendleft(str(x % 2))
+            x = x // 2
+        binary = ''.join(list(queue))
+        list_of_numbers.append('0' * (digits - len(binary)) + binary)
+    return list_of_numbers
+
+
 if __name__ == '__main__':
-    a = StackThroughQueue()
-    a.push(1)
-    a.push(2)
-    a.push(3)
-    a.push(4)
-    print(a.pop())
-    print(a.pop())
-    print(a.pop())
-    print(a.pop())
-    print(a.pop())
+    print(generate_binary_numbers(19))
