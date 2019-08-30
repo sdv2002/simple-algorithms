6efd3a32bef82a7e7f0ac013aa079e21cd9b3cf5
sdv2002 <sdv2002@yandex.ru>
2019-08-30 13:57:43 +0300

    fixed dijkstra

--- Graphs/dijkstra.py
+++ Graphs/dijkstra.py
@@ -3,37 +3,47 @@ from collections import deque
 
 def dijkstra(graph, start, stop):
     """Dijkstra's algorithm is an algorithm for finding
-    the shortest paths between nodes in arbitrary directed
+    the shortest path between nodes in arbitrary-directed
     graphs with unbounded non-negative weights. O(V**2)"""
     queue = deque()
-    path = [stop]
-    short = dict()
-    short[start] = 0
+    label = dict()
+    label[start] = 0
     queue.append(start)
     while queue:
         vertex = queue.popleft()
         for neighbor in graph[vertex]:
-            if neighbor not in short or \
-                    short[neighbor] > short[vertex] + graph[vertex][neighbor]:
-
-                short[neighbor] = short[vertex] + graph[vertex][neighbor]
+            distance = label[vertex] + graph[vertex][neighbor]
+            if neighbor not in label or distance < label[neighbor]:
+                label[neighbor] = distance
                 queue.append(neighbor)
 
+    path = [stop]
+    r = revers_graph(graph)
     while start not in path:
-        for neighbor in graph[path[-1]]:
-            if short[neighbor] == short[path[-1]] - graph[path[-1]][neighbor]:
+        for neighbor in r[path[-1]]:
+            if label[neighbor] == label[path[-1]] - r[path[-1]][neighbor]:
                 path.append(neighbor)
                 break
-    return path[::-1]
+    return path[::-1], label[stop]
+
+
+def revers_graph(graph):
+    """Returns an inverse directed graph."""
+    rev_graph = {vertex: {} for vertex in graph}
+    for vertex in graph:
+        for neighbour in graph[vertex]:
+            rev_graph[neighbour].update({vertex: graph[vertex][neighbour]})
+    return rev_graph
 
 
 if __name__ == '__main__':
-    g = {
-        'a': {'b': 3},
-        'b': {'a': 3, 'c': 2, 'd': 4, 'e': 7},
-        'c': {'b': 2, 'e': 5},
-        'd': {'b': 4, 'e': 1},
-        'e': {'b': 7, 'c': 5, 'd': 1, 'f': 3},
-        'f': {'e': 3}}
-    print(dijkstra(g, 'b', 'e'))
-
+    g = {1: {2: 3, 4: 2},
+         2: {3: 2, 5: 1},
+         3: {6: 1},
+         4: {5: 1, 7: 3},
+         5: {6: 3, 8: 2},
+         6: {9: 3},
+         7: {8: 1},
+         8: {9: 2},
+         9: {}}
+    print(*dijkstra(g, 1, 6))
